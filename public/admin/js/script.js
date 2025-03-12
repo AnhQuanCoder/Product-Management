8b71674e7dfbc32fe66bfde174f29b41b4d17424
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-03-12 23:12:29 +0700

    Tính năng tạo mới sản phẩm (tiếp) (hiển thị ảnh preview)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -155,3 +155,29 @@ if (showAlert) {
     }
 }
 // End show alert
+
+// Upload image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+    const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+    const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+    const closeImage = uploadImage.querySelector("[close-image]");
+
+    if (uploadImageInput && uploadImagePreview && closeImage) {
+        uploadImageInput.addEventListener("change", (e) => {
+            const file = e.target.files[0];
+            if (file && file.type.startsWith("image/")) {
+                uploadImagePreview.src = URL.createObjectURL(file);
+                closeImage.classList.remove("d-none");
+            }
+        });
+
+        closeImage.addEventListener("click", () => {
+            uploadImagePreview.src = "";
+            uploadImageInput.value = "";
+            closeImage.classList.add("d-none");
+        });
+    }
+}
+
+// End upload image
