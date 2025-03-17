7f8f6880995885994b5c847428948baf5edd00d7
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-03-17 22:51:02 +0700

    tiny mce basic (admin)

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-const express = require('express');
-const methodOverride = require('method-override');
-const bodyParser = require('body-parser');
-const flash = require('express-flash');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const express = require("express");
+const path = require("path");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+const flash = require("express-flash");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const app = express();
 
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT;
 
 // create application/x-www-form-urlencoded parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const database = require('./config/database.js');
-const route = require('./routes/client/index.route');
-const routeAdmin = require('./routes/admin/index.route');
+const database = require("./config/database.js");
+const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
 
-const systemConfig = require('./config/system.js');
+const systemConfig = require("./config/system.js");
 
 // connect database
 database.connect();
 
-app.set('views', `${__dirname}/views`);
-app.set('view engine', 'pug');
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "pug");
 
 // Flash
-app.use(cookieParser('ANHQUANCODERBODOITHIENHA'));
-app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(cookieParser("ANHQUANCODERBODOITHIENHA"));
+app.use(session({ cookie: { maxAge: 60000 } }));
 app.use(flash());
 // End Flash
+
+// Tiny MCE
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+// End Tiny MCE
 
 // App local variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
@@ -43,4 +51,4 @@ routeAdmin(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
