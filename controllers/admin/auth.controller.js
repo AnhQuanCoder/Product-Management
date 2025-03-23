fe557a044138133c499567768752a769771e4f02
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-03-23 16:56:18 +0700

    Tính năng đăng nhập (admin)

---
+++ controllers/admin/auth.controller.js
@@ -0,0 +1,42 @@
+const Account = require("../../models/account.model");
+const md5 = require("md5");
+
+const systemConfix = require("../../config/system");
+
+// [GET] /admin/auth/login
+module.exports.login = (req, res) => {
+  res.render("admin/pages/auth/login", {
+    pageTitle: "Đăng nhập",
+  });
+};
+
+// [POST] /admin/auth/login
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  let find = { deleted: false, email: email };
+  const user = await Account.findOne(find);
+
+  if (!user) {
+    req.flash("error", "Email không tồn tại");
+    res.redirect("back");
+    return;
+  }
+
+  if (md5(password) != user.password) {
+    req.flash("error", "Mật khẩu không chính xác");
+    res.redirect("back");
+    return;
+  }
+
+  if (user.status == "inactive") {
+    req.flash("error", "Tài khoản của bạn đã bị khóa");
+    res.redirect("back");
+    return;
+  }
+
+  res.cookie("token", user.token);
+
+  res.redirect(`${systemConfix.prefixAdmin}/dashboard`);
+};
