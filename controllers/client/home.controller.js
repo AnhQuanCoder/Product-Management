4e3968b89e5d4d1ec48dfbdfeec28f0399c535b5
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-03-26 21:49:48 +0700

    Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,23 @@
+const Product = require("../../models/product.model");
+
+const productsHelper = require("../../helper/product");
+
 // [GET] /home
 module.exports.index = async (req, res) => {
+  // Get featured products (Lấy ra sản phẩm nổi bật)
+  let find = {
+    deleted: false,
+    featured: "1",
+    status: "active",
+  };
+  const productsFreatured = await Product.find(find);
+  // End get featured products
+
+  // Dùng priceNewProduct để tính giá mới
+  const newProducts = productsHelper.priceNewProduct(productsFreatured);
+
   res.render("client/pages/home/index.pug", {
     pageTitle: "Trang chủ",
+    productsFreatured: newProducts,
   });
 };
--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,40 +1,39 @@
-const Product = require('../../models/product.model');
+const Product = require("../../models/product.model");
+
+const productsHelper = require("../../helper/product");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
-        //  Lấy data theo điều kiện
-        status: "active",
-        deleted: false
-    }).sort( {position: "desc"} );
+  const products = await Product.find({
+    //  Lấy data theo điều kiện
+    status: "active",
+    deleted: false,
+  }).sort({ position: "desc" });
 
-    // Dùng forEach để thêm 1 thuộc tính mới
-    products.forEach((item) => {
-        item.priceNew = (item.price * (1 - (item.discountPercentage / 100))).toFixed(0);
-    });
+  // Dùng forEach để thêm 1 thuộc tính mới
+  productsNew = productsHelper.priceNewProduct(products);
 
-
-    res.render("client/pages/products/index.pug", {
-        pageTitle: "Danh sách sản phẩm",
-        products: products
-    });
-}
+  res.render("client/pages/products/index.pug", {
+    pageTitle: "Danh sách sản phẩm",
+    products: productsNew,
+  });
+};
 
 // [GET] /products/:slug
 module.exports.detail = async (req, res) => {
-    try {
-        const find = {
-            deleted: false,
-            slug: req.params.slug,
-            status: "active"
-        }
+  try {
+    const find = {
+      deleted: false,
+      slug: req.params.slug,
+      status: "active",
+    };
 
-        const product = await Product.findOne(find);
+    const product = await Product.findOne(find);
 
-        res.render("client/pages/products/detail.pug", {
-            product
-        });
-    } catch (error) {
-        res.redirect("/products");
-    }
-}
+    res.render("client/pages/products/detail.pug", {
+      product,
+    });
+  } catch (error) {
+    res.redirect("/products");
+  }
+};
