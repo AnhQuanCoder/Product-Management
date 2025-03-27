f80267300ff6837e4e59fbdd1e1c4d78c129081a
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-03-27 22:49:33 +0700

    Tính năng thêm sản phẩm vào giỏ hàng

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+// Show alert
+const showAlert = document.querySelector("[show-alert]");
+if (showAlert) {
+  const time = parseInt(showAlert.getAttribute("data-time"));
+
+  const closeAlert = showAlert.querySelector("[close-alert]");
+
+  setTimeout(() => {
+    showAlert.classList.add("alert-hidden");
+  }, time);
+
+  if (closeAlert) {
+    closeAlert.addEventListener("click", () => {
+      showAlert.classList.add("alert-hidden");
+    });
+  }
+}
+// End show alert
