83dca373167ebc64a681ee401bfd417c7a235f5a
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-03-28 20:15:41 +0700

    Làm trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,35 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+
+const productHelper = require("../../helper/product");
+
+// [GET] cart/
+module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId;
+
+  const cart = await Cart.findOne({ _id: cartId });
+
+  if (cart.products.length > 0) {
+    for (const item of cart.products) {
+      const productInfo = await Product.findOne({ _id: item.product_id });
+      item.productInfo = productInfo;
+
+      productInfo.priceNew = productHelper.priceNewProduct(item.productInfo);
+
+      item.totalPrice = item.quantity * productInfo.priceNew;
+    }
+  }
+
+  cart.totalPrice = cart.products.reduce(
+    (sum, item) => sum + item.totalPrice,
+    0
+  );
+
+  res.render("client/pages/cart/index.pug", {
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart,
+  });
+};
 
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
