a7082a19c1cb449f500e435b48e66a7ba065ada7
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-03-29 14:16:05 +0700

    Tính năng đăng nhập (client)

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -32,3 +32,40 @@ module.exports.registerPost = async (req, res) => {
 
   res.redirect("/");
 };
+
+// [GET] /user/login
+module.exports.login = (req, res) => {
+  res.render("client/pages/user/login", {
+    pageTitle: "Đăng nhập tài khoản",
+  });
+};
+
+// [POST] /user/loginPost
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await User.findOne({ email: email, deleted: false });
+
+  if (!user) {
+    req.flash("error", `Email ${email} không tồn tại !`);
+    res.redirect(`back`);
+    return;
+  }
+
+  if (md5(password) != user.password) {
+    req.flash("error", `Sai mật khẩu, vui lòng thử lại !`);
+    res.redirect(`back`);
+    return;
+  }
+
+  if (user.status == "inactive") {
+    req.flash("error", `Tài khoản của bạn đã bị khóa !`);
+    res.redirect(`back`);
+    return;
+  }
+
+  res.cookie("tokenUser", user.tokenUser);
+
+  res.redirect(`/`);
+};
